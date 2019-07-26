1af5ef86935b917fc078076e8f30d1b57965f695
Watson121 <33036071+Watson121@users.noreply.github.com>
2019-07-27 00:56:06 +0100

    Add files via upload

---
+++ PlayerPawn.cpp
@@ -0,0 +1,176 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PlayerPawn.h"
+#include "RubyPistolV2.h"
+#include "FrenchRifle.h"
+#include "MedicBag.h"
+#include "Engine.h"
+#include "Components/InputComponent.h"
+
+// Sets default values
+APlayerPawn::APlayerPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	playerRoot = CreateDefaultSubobject<USceneComponent>(TEXT("Player Root"));
+	RootComponent = playerRoot;
+
+	vrOrigin = CreateDefaultSubobject<USceneComponent>(TEXT("VROrigin"));
+	vrOrigin->SetupAttachment(RootComponent);
+
+	camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	camera->SetupAttachment(vrOrigin);
+
+	camera->bLockToHmd = true;
+
+	DefaultCharacterHeight = 160;
+	FadeOutDuration = 0.1f;
+	FadeInDuraction = 0.2f;
+
+	leftHand = false;
+	rightHand = false;
+
+	currentPlayerHealth = 70;
+	fullPlayerHealth = 100;
+
+	BaseEyeHeight = 180;
+}
+
+// Called when the game starts or when spawned
+void APlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+	HMDDeviceName = GetHMDDeviceName();
+	
+	for (TActorIterator<ARubyPistolV2> ActorItr(GetWorld()); ActorItr; ++ActorItr) {
+		rubyPistol = *ActorItr;
+	}
+
+	for (TActorIterator<AFrenchRifle> ActorItr(GetWorld()); ActorItr; ++ActorItr) {
+		frenchRifle = *ActorItr;
+	}
+
+	
+}
+
+// Called every frame
+void APlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	//GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Yellow, TEXT("HELLO WORLD"));
+}
+
+// Called to bind functionality to input
+void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	InputComponent->BindAction("Reload", IE_Pressed, this, &APlayerPawn::SendReloadMessage);
+	InputComponent->BindAction("FireLeft", IE_Pressed, this, &APlayerPawn::SendLeftFireMessage);
+	InputComponent->BindAction("FireRight", IE_Pressed, this, &APlayerPawn::SendRightFireMessage);
+
+}
+
+
+
+//SEND MESSAGE FUNCTIONS
+
+void APlayerPawn::SendReloadMessage() {
+	//if (pistolPickedUp) {
+	//	GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Yellow, TEXT("HELLO WORLD"));
+	//	rubyPistol->Reload();
+	//}
+
+	//if (riflePickedUp) {
+	//	GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Yellow, TEXT("HELLO WORLD"));
+	//	frenchRifle->Reload();
+	//}
+	
+	if (medicBagPickedUp) {
+		HealPlayer(20);
+		medicBag->DestroyThis();
+	}
+}
+
+void APlayerPawn::SendLeftFireMessage() {
+	if (pistolPickedUp) {
+		GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Yellow, TEXT("HELLO WORLD"));
+		leftHand = true;
+		rubyPistol->FireLeft();
+	}
+
+	if (riflePickedUp) {
+		GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Yellow, TEXT("HELLO WORLD"));
+		leftHand = true;
+		frenchRifle->FireLeft();
+	}
+
+}
+
+void APlayerPawn::SendRightFireMessage() {
+	if (pistolPickedUp) {
+		GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Yellow, TEXT("HELLO WORLD"));
+		rubyPistol->FireRight();
+	}
+
+	if (riflePickedUp) {
+		GEngine->AddOnScreenDebugMessage(-1, 0.5f, FColor::Yellow, TEXT("HELLO WORLD"));
+		frenchRifle->FireRight();
+	}
+
+}
+
+
+//PLAYERS STATS
+
+void APlayerPawn::HealPlayer(float healAmount) {
+	currentPlayerHealth = currentPlayerHealth + healAmount;
+}
+
+void APlayerPawn::TakeAwayHealth() {
+	currentPlayerHealth = currentPlayerHealth - 5;
+}
+
+
+//GET FUNCTIONS
+
+bool APlayerPawn::GetPistol() {
+	return pistolPickedUp;
+}
+
+FName APlayerPawn::GetHMDDeviceName() {
+
+//This function gets the HMD Device Name
+
+	FName deviceName;
+
+	if (GEngine->XRSystem.IsValid()) {
+		deviceName = GEngine->XRSystem->GetSystemName();
+	}
+
+	return deviceName;
+}
+
+bool APlayerPawn::GetLeftHand() {
+	return leftHand;
+}
+
+bool APlayerPawn::GetRightHand() {
+	return rightHand;
+}
+
+//SETTER FUNCTIONS
+
+void APlayerPawn::SetMedicBag(class AMedicBag* medic) {
+	medicBag = medic;
+}
+
+
+
+
+
+
+
---
+++ PlayerPawn.h
@@ -0,0 +1,150 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine.h"
+#include "IXRSystemAssets.h"
+#include "IXRTrackingSystem.h"
+#include "HeadMountedDisplay.h"
+#include "HeadMountedDisplayBase.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerPawn.generated.h"
+
+class ARubyPistolV2;
+class AFrenchRifle;
+class AMedicBag;
+
+UCLASS()
+class TRENCHCOATSGAME_API APlayerPawn : public APawn
+{
+	GENERATED_BODY()
+
+	
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerPawn();
+
+private:
+
+	// Called when the game starts or when spawned
+	
+
+
+
+
+
+protected:
+
+	virtual void BeginPlay() override;
+
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+	void SendReloadMessage();
+	void SendLeftFireMessage();
+	void SendRightFireMessage();
+	void HealPlayer(float healAmount);
+
+	UFUNCTION(BlueprintCallable)
+		void TakeAwayHealth();
+
+	UFUNCTION()
+	virtual bool GetPistol();
+
+	UFUNCTION()
+	FName GetHMDDeviceName();
+
+	void SetMedicBag(class AMedicBag* medic);
+
+
+	//Functions Related to Player Health
+	//UFUNCTION()
+	//	virtual int GetPlayerHealth() override;
+	//UFUNCTION()
+	//	virtual void TakePlayerHealth(int damage) override;
+	//UFUNCTION()
+	//	virtual void CheckPlayerHealth() override;
+
+	virtual bool GetLeftHand();
+	virtual bool GetRightHand();
+
+	//Scene Componenets
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		USceneComponent* playerRoot;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		USceneComponent* vrOrigin;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		UCameraComponent* camera;
+
+	//floats
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		float FadeOutDuration;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		float FadeInDuraction;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		float ThumbDeadzone;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		float DefaultCharacterHeight;
+
+	//ints
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		int PistolAmmo;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		int RifleAmmo;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		float currentPlayerHealth;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		float fullPlayerHealth;
+
+	//Bools
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		bool IsTeleporting;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		bool RightStickDown;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		bool LeftStickDown;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		bool UseControllerRollToRotate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool leftHand;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool rightHand;
+
+	//Fname
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FName HMDDeviceName;
+
+	UPROPERTY(EditAnywhere)
+	ARubyPistolV2* rubyPistol;
+	UPROPERTY(EditAnywhere)
+	AFrenchRifle* frenchRifle;
+	UPROPERTY(EditAnywhere)
+	AMedicBag* medicBag;
+
+	//Pick up Objects
+
+	//Weapons
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		bool pistolPickedUp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		bool riflePickedUp;
+
+
+	//Other Items
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		bool medicBagPickedUp;
+
+	
+
+};
